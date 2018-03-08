88189613250d58c8fe5eaf8e915f73c1b402c238
Wiseley Wu <wiseleywu@gmail.com>
2018-03-08 11:47:25 -0800

    add new api call to obtain stations data

--- bart-tracker/api/get_advisory.py
+++ bart-tracker/api/get_advisory.py
@@ -57,7 +57,7 @@ def get_advisory(bsa_type, debug=False):
         message = '"{}"'.format(message)
         sms = '"{}"'.format(sms)
 
-        posted = access_xml_element(bsa, bsa_keys.get('posted'))
+        posted = access_xml_element(bsa, bsa_keys.get('posted'), attr='text')
 
         if debug:
             return response
@@ -113,21 +113,67 @@ def get_elevator_outage(bsa_type, debug=False):
         else:
             return [date, time, bsa_id, equipment_type, station, message, sms]
 
-elev_response = get_elevator_outage(SECTION[0])
-elev_response = ['None' if i is None else i for i in elev_response]
+def get_station(API_KEY, station):
+    api_location = SITE + 'stn.aspx?cmd=stns&key={}'.format(API_KEY)
 
-bsa_response = get_advisory(SECTION[0])
-bsa_response = ['None' if i is None else i for i in bsa_response]
+    try:
+        response = requests.get(api_location, timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.Timeout:
+        sys.exit('Request Timeout. No information obtained')
+    except requests.exceptions.HTTPError as e:
+        sys.exit(e)
 
-count_response = get_train_count(SECTION[0])
-count_response = ['None' if i is None else i for i in count_response]
+    if response.status_code == requests.codes.ok:
+        root = ET.fromstring(response.content)
+
+        msg = []
+        for child in root.iter():
+            child.text and msg.append(child.text.lower())
+        if 'invalid key' in msg:
+            sys.exit('Error found in response: {}'.format(','.join(msg)))
+
+        keys = {}
+        for i, j in enumerate(root):
+            keys[j.tag] = i
+
+        stations = access_xml_element(root, keys.get('stations'))
+
+        stn_keys = {}
+        for i, j in enumerate(stations):
+            stn_atr = {}
+            for a, b in enumerate(j):
+                stn_atr[b.tag] = a
+            stn_keys[access_xml_element(j, stn_atr.get('abbr'), attr='text')] = i
+
+        if station.lower() == 'all':
+            return [{c.tag: c.text for c in stn} for stn in stations]
+        else:
+            req_stn = access_xml_element(stations, stn_keys.get(station))
+            if req_stn:
+                return {c.tag: c.text for c in req_stn}
+            else:
+                raise KeyError('Station name does not exist')
+
+    else:
+        sys.exit("API Error! HTTP response code {} received".format(response.status_code))
+
+if __name__ == '__main__':
+    elev_response = get_elevator_outage(SECTION[0])
+    elev_response = ['None' if i is None else i for i in elev_response]
+
+    bsa_response = get_advisory(SECTION[0])
+    bsa_response = ['None' if i is None else i for i in bsa_response]
+
+    count_response = get_train_count(SECTION[0])
+    count_response = ['None' if i is None else i for i in count_response]
 
 
-with open(os.path.join(SAVE_PATH, 'advisory.txt'), 'a') as f:
-    f.write(','.join(bsa_response)+'\n')
+    with open(os.path.join(SAVE_PATH, 'advisory.txt'), 'a') as f:
+        f.write(','.join(bsa_response)+'\n')
 
-with open(os.path.join(SAVE_PATH, 'train_count.txt'),'a') as f:
-    f.write(','.join(count_response)+'\n')
+    with open(os.path.join(SAVE_PATH, 'train_count.txt'),'a') as f:
+        f.write(','.join(count_response)+'\n')
 
-with open(os.path.join(SAVE_PATH, 'elevator_status.txt'),'a') as f:
-    f.write(','.join(elev_response)+'\n')
+    with open(os.path.join(SAVE_PATH, 'elevator_status.txt'),'a') as f:
+        f.write(','.join(elev_response)+'\n')
